c51e014271dba7425288b3283826b4bd7f29bb3c
Dav <david.sanchez.martinez@outlook.com>
2026-04-18 19:02:53 +0200

    feat: add guard against user input and store fetched data in an array

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const form = document.getElementById('search-bar')
 const searchInput = document.getElementById('search-input')
 const searchBtn = document.getElementById('search-btn')
+const showsSection = document.getElementById('shows-section')
+
 
 
 
 const handleClick = async (e) => {
   e.preventDefault()
 
-  const inputValue = searchInput.value
-   
-  const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=96b4f733b8a3836c9dfb5ea5e1034a79&query=${encodeURIComponent(inputValue)}`, options)
+  const inputValue = searchInput.value.trim() 
+
+  if (!inputValue) {
+    console.log('No user input')
+    return
+  }
+
+  const res = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=96b4f733b8a3836c9dfb5ea5e1034a79&query=${encodeURIComponent(inputValue)}`)
   const data = await res.json()
 
+  const showsArr = data.results
   console.log(data)
+  showsSection.innerHTML = generateShowHtml(showsArr)
 }
 
 
