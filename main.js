ad20e61d8f89950e2a83007c22584cbe9ac341d8
Dav <david.sanchez.martinez@outlook.com>
2026-04-20 14:09:56 +0200

    feat: render shows card dynamically

--- main.js
+++ main.js
@@ -4,12 +4,37 @@ const searchBtn = document.getElementById('search-btn')
 const showsSection = document.getElementById('shows-section')
 
 
+let showsArr = []
 
+// Pure helper (generate HTML)
+
+const generateShowHtml = (shows) => {
+  let feedHtml = ''
+
+  shows.forEach(show => {
+    feedHtml += `
+      <div class="show-card" data-id="${show.id}">
+        <img class="show-card-img" src="${show.poster_path}"/>
+        <div class="show-card-info">
+          <h2 class="show-title">
+            ${show.name || show.title} <span>${show.vote_average} </span>
+          </h2>
+          <div class="show-details">
+            <span>${show.media_type}</span>
+            <button>Watchlist</button>
+          </div>
+          <p>${show.overview}</p>
+        </div>
+      </div>
+    `
+  })
+  return feedHtml
+}
 
 const handleClick = async (e) => {
   e.preventDefault()
 
-  const inputValue = searchInput.value.trim() 
+  const inputValue = searchInput.value.trim()
 
   if (!inputValue) {
     console.log('No user input')
@@ -19,9 +44,12 @@ const handleClick = async (e) => {
   const res = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=96b4f733b8a3836c9dfb5ea5e1034a79&query=${encodeURIComponent(inputValue)}`)
   const data = await res.json()
 
-  const showsArr = data.results
-  console.log(data)
-  showsSection.innerHTML = generateShowHtml(showsArr)
+  showsArr = data.results
+  // showsSection.innerHTML = generateShowHtml(showsArr)
+  console.log(showsArr)
+
+  console.log(curateShows(showsArr))
+
 }
 
 
