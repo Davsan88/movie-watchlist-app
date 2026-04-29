013fb3509ea558555fc2b6a7bd83f0fe60726d43
Dav <david.sanchez.martinez@outlook.com>
2026-04-29 12:33:32 +0200

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -5,20 +5,31 @@ export const generateShowHtml = (shows, mode) => {
     let feedHtml = ''
 
     shows.forEach(show => {
-
         feedHtml += `
+
     <div class="show-card">
-    <img class="show-card-img" src="https://image.tmdb.org/t/p/w500/${show.posterPath}"/>
-    <div class="show-card-info">
-    <h2 class="show-title">
-    ${show.label} <span>${show.rating} </span>
-    </h2>
-    <div class="show-details">
-    <span class="media-type-span">${show.mediaType}</span>
-    <button class="action-btn" data-action="${action}"data-id="${show.id}">${buttonText}</button>
-    </div>
-    <p>${show.overview}</p>
-    </div>
+        <img class="show-card-img" src="https://image.tmdb.org/t/p/w500/${show.posterPath}"/>
+        
+        <div class="show-card-info">
+            <h2 class="show-title">
+                ${show.label} 
+                <span> 
+                    ${show.rating} 
+                </span>
+            </h2>
+            <div class="show-details">
+                <span class="media-type-span">
+                    ${show.mediaType}
+                </span>
+                <button 
+                    class="action-btn" 
+                    data-action="${action}" 
+                    data-id="${show.id}">
+                        ${buttonText}
+                </button>
+            </div>
+            <p>${show.overview}</p>
+        </div>
     </div>
     `
     })
